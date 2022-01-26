b25691127fa50b68eaa37b7a23d7a4790bc13861
SEIISUII <ruvi727@gmail.com>
2022-01-26 22:13:56 +0900

    2주차

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,49 +5,72 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
-    public float JumpPower = 3;
-    private Rigidbody2D rigid;
-    private bool facingRight = true;
     private Vector3 vector;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;   //픽셀 단위로 이동
+
+    private bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+
+        else
+        {
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+        }
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while (currentWalkCount < walkCount)
+        {
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+
+            if(applyRunFlag)
+            {
+                currentWalkCount++;
+            }
+
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-       float hor = Input.GetAxis("Horizontal");        //이동 : 1 = 오른쪽, -1 = 왼쪽
- 
-        transform.Translate(Vector3.right * speed * hor * Time.deltaTime);
-        if (Input.GetAxis("Horizontal") > 0.5f || Input.GetAxis("Horizontal") < -0.5f) {    // 만약 왼쪽 혹은 오른쪽 이동 중 0.5f 이상인 경우
- 
-            if(Input.GetAxis("Horizontal") > 0.5f && !facingRight)      //facingRight 가 false이면서 오른쪽 이동키 누른 경우.
-            {
-                Flip();
-                facingRight = true;
-            }
-            else if(Input.GetAxis("Horizontal") < -0.5f && facingRight)     // gacingRight 가 true이면서 왼쪽 이동키 누른 경우.
-            {
-                Flip();
-                facingRight = false;
-            }
-        }
- 
-        if (Input.GetKey(KeyCode.Space))        //스페이스바 키 누를 시 점프
+        if(canMove)
         {
-            rigid.velocity = Vector3.up * JumpPower;
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
         }
-    }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;             // 1 = 오른쪽 방향, -1 = 왼쪽방향
-        transform.localScale = theScale;
-    }
+    }      
 }

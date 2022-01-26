c33b24516a83dc8270dab57011649771e6c6673a
SEIISUII <ruvi727@gmail.com>
2022-01-26 16:58:54 +0900

    2주차

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed = 4;
+    public float JumpPower = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, v, 0);
         
+        transform.position += dir * speed * Time.deltaTime;
     }
 }

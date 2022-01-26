fbf39d9220dd362b2896932a39a42be348d7734c
SEIISUII <ruvi727@gmail.com>
2022-01-26 18:36:04 +0900

    2주차

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 4;
+    public float speed;
     public float JumpPower = 3;
+    private Rigidbody2D rigid;
+    private bool facingRight = true;
+    private Vector3 vector;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       float hor = Input.GetAxis("Horizontal");        //이동 : 1 = 오른쪽, -1 = 왼쪽
+ 
+        transform.Translate(Vector3.right * speed * hor * Time.deltaTime);
+        if (Input.GetAxis("Horizontal") > 0.5f || Input.GetAxis("Horizontal") < -0.5f) {    // 만약 왼쪽 혹은 오른쪽 이동 중 0.5f 이상인 경우
+ 
+            if(Input.GetAxis("Horizontal") > 0.5f && !facingRight)      //facingRight 가 false이면서 오른쪽 이동키 누른 경우.
+            {
+                Flip();
+                facingRight = true;
+            }
+            else if(Input.GetAxis("Horizontal") < -0.5f && facingRight)     // gacingRight 가 true이면서 왼쪽 이동키 누른 경우.
+            {
+                Flip();
+                facingRight = false;
+            }
+        }
+ 
+        if (Input.GetKey(KeyCode.Space))        //스페이스바 키 누를 시 점프
+        {
+            rigid.velocity = Vector3.up * JumpPower;
+        }
+    }
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(h, v, 0);
-        
-        transform.position += dir * speed * Time.deltaTime;
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;             // 1 = 오른쪽 방향, -1 = 왼쪽방향
+        transform.localScale = theScale;
     }
 }

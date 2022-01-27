7a4fd3b5463f5998f5c2c3f36b890c6029a39b3b
SEIISUII <ruvi727@gmail.com>
2022-01-27 17:03:10 +0900

    3주차

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,17 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour
 {
-    public string playScene = "PlayScene";
+    public Image image;
+    public GameObject startButton;
+    public GameObject endButton;
+    public GameObject copyright;
+
+    public static bool isEndFadeOut = false;
+    public string lobbyScene = "LobbyScene";
     public string loadScene = "LoadingScene";
 
     public void ClickStart()
     {
+        isEndFadeOut = false;
+        Debug.Log("버튼 클릭");
+        StartCoroutine(FadeCoroutine());
+        
+        startButton.SetActive(false);
+        endButton.SetActive(false);
+        copyright.SetActive(false);
+
+        StartCoroutine(CheckFadeOutCoroutine());
+        
+    }
+
+    IEnumerator FadeCoroutine()
+    {
+        float fadeCount = 0;
+        while(fadeCount < 1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
+            isEndFadeOut = true;
+        }
+    }
+
+    IEnumerator CheckFadeOutCoroutine()
+    {
+        while (isEndFadeOut == false)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.5f);
         Debug.Log("게임 시작");
-        SceneManager.LoadScene(playScene);
+        SceneManager.LoadScene(lobbyScene);
     }
 
     public void ClickLoad()

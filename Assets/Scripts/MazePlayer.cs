b8c4771dfa7289ed990efff87205a964b610d3da
easyone824 <98134278+easyone824@users.noreply.github.com>
2022-02-06 19:44:21 +0900

    EscapeGame

--- Assets/Scripts/MazePlayer.cs
+++ Assets/Scripts/MazePlayer.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MazePlayer : MonoBehaviour
 {
 
-    float dc;
+    public float dc;
     public GameObject door;
+
+    public Text counttext;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -51,6 +56,7 @@ public class MazePlayer : MonoBehaviour
         if (collision.gameObject.tag == "DreamCatcher")
         {
             dc++;
+            counttext.text = dc.ToString();
         }
     }
 

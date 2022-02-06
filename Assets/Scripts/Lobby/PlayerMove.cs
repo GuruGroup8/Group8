887315a52026544ec30d949383fc53c17f4804cb
SEIISUII <ruvi727@gmail.com>
2022-02-06 20:13:25 +0900

    playermove

--- Assets/Scripts/Lobby/PlayerMove.cs
+++ Assets/Scripts/Lobby/PlayerMove.cs
@@ -5,9 +5,7 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-    private BoxCollider2D = boxCollider;
-    public LayerMask layerMask;
-
+ 
     public float speed;
     private Vector3 vector;
 
@@ -25,7 +23,6 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -52,20 +49,6 @@ public class PlayerMove : MonoBehaviour
 
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
-
-            RayCastHit2D hit;
-
-            Vector2 start = transform.position;  //a지점(캐릭터 위치)
-            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);    //b지점(캐릭터가 이동하고자 하는 위치값)
-
-            boxCollider.enabled = false;
-            hit = Physics2D.Linecast(start, end, layerMask);
-            boxCollider.enabled = true;
-
-            if(hit.transform != null)
-            {
-                break;
-            }
 
             animator.SetBool("Walking", true);
 

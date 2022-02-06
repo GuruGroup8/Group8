7cd968f45e8e1ac0ce6e4d97ef4b1273fec0a1b8
SEIISUII <ruvi727@gmail.com>
2022-02-06 20:47:34 +0900

    shootingoption

--- Assets/Scripts/ShootingGame/ClickManager.cs
+++ Assets/Scripts/ShootingGame/ClickManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonClick : MonoBehaviour
+public class ClickManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

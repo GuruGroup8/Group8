0cc54f28201c934c385c2e5755fc598a430ef466
easyone824 <98134278+easyone824@users.noreply.github.com>
2022-02-06 22:06:08 +0900

    e

--- Assets/Scripts/MazeGame/MazePlayer.cs
+++ Assets/Scripts/MazeGame/MazePlayer.cs
@@ -11,17 +11,19 @@ public class MazePlayer : MonoBehaviour
 
     public Text counttext;
 
-
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(0.03f, 0, 0);
